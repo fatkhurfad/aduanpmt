fc4d27a93939960e963a30f473493f05201fd0e4
fatkhurfad <fatkhurfadarman@gmail.com>
2025-06-07 01:19:17 +0700

    Update auth.py

--- modules/auth.py
+++ modules/auth.py
@@ -2,24 +2,23 @@ import streamlit as st
 from modules.config import t
 
 def show_login():
-    st.title(t("welcome", st.session_state.lang))
-    st.markdown(t("login", st.session_state.lang))
+    st.title(t("welcome"))
+    st.markdown(t("login"))
     with st.form("login_form"):
-        username = st.text_input(t("username", st.session_state.lang))
-        password = st.text_input(t("password", st.session_state.lang), type="password")
-        submitted = st.form_submit_button(t("login_button", st.session_state.lang))
+        username = st.text_input(t("username"))
+        password = st.text_input(t("password"), type="password")
+        submitted = st.form_submit_button(t("login_button"))
         if submitted:
-            # Ganti logika berikut dengan autentikasi yang kamu mau
-            if username == "admin" and password == "surat123":
+            if username == "aku" and password == "adalah":
                 st.session_state.login_state = True
                 st.session_state.username = username
                 st.session_state.logout_message = False
                 st.experimental_rerun()
             else:
-                st.error(t("login_fail", st.session_state.lang))
+                st.error(t("login_fail"))
 
 def show_logout():
-    if st.sidebar.button(t("logout_button", st.session_state.lang)):
+    if st.sidebar.button(t("logout_button")):
         st.session_state.logout_message = True
         st.session_state.login_state = False
         st.session_state.username = ""
